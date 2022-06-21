80b2a198bcdf2a369b1e3dcc5ad1d03217b7be8c
Jonathan suarez <jonathansuarez@Jonathans-MBP.lan>
2022-06-21 18:09:15 -0400

    defining js stuff

---
+++ arthurmock/infunction.js
@@ -0,0 +1,30 @@
+//function js
+/*
+
+1.what is a function
+
+ans: resuable block of code designed to perform a single purpose
+
+
+2.defining a function
+
+ans:
+A.Function Declaration
+
+B.Function Expression
+
+C.Arrow Functions
+
+3.syntax of function
+
+4.example of a function
+
+5.parameters of function
+
+6.explanation of function
+
+7.calling of a function
+
+8.return statement
+
+*/
---
+++ arthurmock/interview.js
@@ -0,0 +1,159 @@
+//define the following below
+
+
+/*   
+1.JavaScript Data Types
+
+ans: 
+primitive data types
+
+-boolean - true or false
+-null - nothing but i set it to nothing 
+-undefined - i never gave something a value so its undefined
+-number - decimals, whole numbers, NaN, infinity, -infinity
+-string - anything wraped in quotes "" ''
+-symbol -
+
+if a variable is not any of the six data types above it's a object
+which can contain any number of primitive value and/or other objects.
+
+object- is a referance type.
+array
+object
+function
+regEx
+date
+
+2.Difference Between Const Let and Var
+
+ans: 
+difference between var and let/constis what we call scope.
+var has function scope known as local scope and let and const
+have limited block scope. block scope reduces the chance of accidentally
+changing the variable value!!!! let you can change variale can be re-assigned while const can't
+
+3.Pass By Value vs Pass By Reference
+
+ans: 
+objects are refernce types 
+
+4.Map , Filter and Reduce ( Mutation or not)
+
+ans:
+Map -
+
+Filter -
+
+Reduce -
+
+5.Falsey Values
+
+ans:
+1. false - keyword false
+2. 0
+3. -0
+4. '',"",`` - empty string falsey
+5. null - absence of any value
+6. undefined
+7. NaN - not a number
+
+
+6.Global Variables
+
+ans: 
+Variables declared Globally (outside any function) have Global Scope.
+
+Global variables can be accessed from anywhere in a JavaScript program.
+
+Variables declared with var, let and const are quite similar when declared outside a block.
+
+They all have Global Scope.
+
+In JavaScript, objects and functions are also variables.
+
+7.This
+
+8.double equals vs triple equals
+
+ans: double == if two things are not the same type make them both the same then compare.
+while triple === has to be strictly the same
+
+9.Coercion
+ 
+ans:
+the process of converting value from one type to another (such as string to number, object
+ to boolean, and so on). Any type, be it primitive or an object, is a valid subject for type coercion. 
+
+10.typeof
+
+ans:
+returns a string indicating the type of the unevaluated operand.
+
+11.delete (not the http method)
+
+12.Object Notations
+ans:
+JSON
+
+13.Strict Mode
+
+14.Anonymous Functions
+
+15.Callbacks
+
+16.Closure
+
+17.instanceof
+
+18.IIFE
+
+19.HTML5 APIS
+
+20.Destructuring
+
+21.Rest and Spread
+
+22.Arrow Function differences
+
+23.HTTP Methods
+
+24.HTTP Status Codes
+
+25.HTTP 2 and HTTP 3
+
+26.Restful Routes
+
+27.Promises
+
+28.Async Await
+
+29.CSS Specificity
+
+30.Semantic HTML
+
+31.How do you make a Circle in CSS
+
+32.What is Z-index
+
+33.Vendor Prefixes Purpouse
+
+34.CSS Grid and Flexbox
+
+35.Speed Up a Slow App
+
+36.CRUD
+
+37.CRAP
+
+38.SOLID
+
+39.OOP
+
+40.Functional Programming
+
+41.Recursion
+
+42.SPA
+
+43.MPA
+    */
